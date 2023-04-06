1566f4ae4a9c1c7215db3d4a53f3e63a27dc9445
unknown <monniewanjema@gmail.com>
2023-04-06 19:15:01 +0300

    Tdd test

---
+++ solver.rb
@@ -0,0 +1,26 @@
+class Solver
+    def factorial (n)
+        result = 1
+    return result if [0, 1].include?(n)
+    raise StandardError, 'Number cannot be negative' if n.negative?
+
+    (1..n).each { |i| result *= i }
+    result
+    end
+
+    def reverse (word)
+        word.reverse
+    end
+
+    def fizzbuzz (n)
+        if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+    elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
+    end
+    end
+end
